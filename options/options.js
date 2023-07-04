7ee237a311b37ab223584b7d6fca4a30025a0592
Crowdbotics <team@crowdbotics.com>
2023-07-04 00:17:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quiet-night-41888",
+  url: "https://quiet-night-41888.botics.co",
+  api: "https://quiet-night-41888.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
